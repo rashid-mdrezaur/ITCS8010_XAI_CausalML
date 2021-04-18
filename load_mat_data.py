63e4dede7adc303ec616fdfa7cf03bb69b9ca53f
jmelton22 <jmelton22@gmail.com>
2021-04-18 13:00:15 -0400

    MinMax scale data before saving as csv

--- load_mat_data.py
+++ load_mat_data.py
@@ -3,11 +3,17 @@
 import numpy as np
 import scipy.io
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 input_mat = scipy.io.loadmat('data/model3_inputs.mat')['model3_inputs']
 target_mat = scipy.io.loadmat('data/bldgs_targets.mat')['bldgs_targets']
 
+# TODO: Figure out column meanings. Check if any are continuous or all are discrete
+#  Matlab script uses MinMax for all columns
+scaler = MinMaxScaler()
+input_mat = scaler.fit_transform(input_mat)
+
 data = pd.DataFrame(input_mat)
 data['target'] = np.argmax(target_mat, axis=1)
 
-data.to_csv('data/data_df.csv', index=False, header=0)
+data.to_csv('data/data_df.csv', index=False, header=True)
