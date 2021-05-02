5183acd6f81cc3ee7d8386c39d4de4f7393d500e
jmelton22 <jmelton22@gmail.com>
2021-05-02 18:30:12 -0400

    Update to run train/test cv trials for model

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 import pandas as pd
-import numpy as np
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score, mean_squared_error, mean_absolute_error
+from statistics import mean, stdev
 
 import torch
 from torch.utils.data import DataLoader
@@ -11,17 +12,18 @@ from torch import optim
 import torch.nn.functional as F
 
 from dataset import BldgDataset
-from model import ClassificationModel, RegressionModel
+from model import ClassificationModel
 
 
 def main():
-    model_name = 'model_test_m8_cols'
-    model_type = 'classification'
+    model_name = 'model_08_cols_w_dist'
+    os.makedirs(f'models/{model_name}', exist_ok=True)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     batch_size = 8
-    EPOCHS = 25
-    lr = 3e-3
+    EPOCHS = 20
+    lr = 5e-3
     l2_reg = 0.1
+    n_trials = 10
 
     data = pd.read_csv('data/preprocessed.csv', header=0)
     model_8_cols = [
@@ -51,112 +53,101 @@ def main():
 
     data = data[model_8_cols + ['target']]
 
-    # TODO: Replace with cross validation?
-    test_df = data.sample(frac=0.2)
-    data.drop(test_df.index, inplace=True)
+    acc_list, mse_list, mae_list, mae_missed_list = [], [], [], []
+    for trial in range(n_trials):
 
-    train_data = BldgDataset(data)
-    test_data = BldgDataset(test_df)
+        test_df = data.sample(frac=0.2)
+        train_df = data.drop(test_df.index)
 
-    train_loader = DataLoader(train_data,
-                              batch_size=batch_size,
-                              shuffle=True,
-                              pin_memory=True)
-    test_loader = DataLoader(test_data,
-                             batch_size=batch_size*2,
-                             shuffle=False)
+        train_data = BldgDataset(train_df)
+        test_data = BldgDataset(test_df)
 
-    if model_type == 'regression':
-        model = RegressionModel(in_feats=train_data.num_feats(),
-                                hidden_dim=10,
-                                dropout=0.2,
-                                activation='relu')
-    else:
+        train_loader = DataLoader(train_data,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  pin_memory=True)
+        test_loader = DataLoader(test_data,
+                                 batch_size=batch_size*2,
+                                 shuffle=False)
+
         model = ClassificationModel(in_feats=train_data.num_feats(),
                                     hidden_dim=10,
                                     n_classes=train_data.num_classes(),
                                     dropout=0.2,
                                     activation='relu')
-    optimizer = optim.Adam(model.parameters(),
-                           lr=lr,
-                           weight_decay=l2_reg)
+        optimizer = optim.Adam(model.parameters(),
+                               lr=lr,
+                               weight_decay=l2_reg)
 
-    model.to(device)
-    for epoch in range(EPOCHS):
+        model.to(device)
+        for epoch in range(EPOCHS):
 
-        data_iter = tqdm(
-            train_loader,
-            desc=f'Epoch: {epoch:02}',
-            total=len(train_loader)
-        )
+            data_iter = tqdm(
+                train_loader,
+                desc=f'Epoch: {epoch:02}',
+                total=len(train_loader)
+            )
 
-        model.train()
-        avg_loss = 0.
-        for i, (X, y) in enumerate(data_iter):
-            X, y = X.to(device), y.to(device)
-            optimizer.zero_grad()
+            model.train()
+            avg_loss = 0.
+            for i, (X, y) in enumerate(data_iter):
+                X, y = X.to(device), y.to(device)
+                optimizer.zero_grad()
 
-            logits = model(X)
-            # TODO: Find a loss function for ordinal classification
-            if model_type == 'regression':
-                loss = F.mse_loss(logits.squeeze(), y.type(torch.float32))
-            else:
+                logits = model(X)
+
+                # TODO: Find a loss function for ordinal classification. Try adding distance penalty for ordinality?
                 loss = F.cross_entropy(logits, y)
                 # y_one_hot = F.one_hot(y, num_classes=train_data.num_classes())
                 # loss += 1e-4 * F.mse_loss(logits, y_one_hot.float())
 
-            # TODO: Try adding distance penalty for ordinality?
-            # y_one_hot = F.one_hot(y, num_classes=train_data.num_classes())
-            # # preds = torch.argmax(model(X), dim=-1)
-            # pred_distance = torch.dist(logits, y_one_hot.float(), p=2)
-            #
-            # loss += 1e-4 * pred_distance
+                loss.backward()
+                optimizer.step()
 
-            loss.backward()
-            optimizer.step()
+                avg_loss += loss.item()
+                data_iter.set_postfix({
+                    'avg_loss': avg_loss / (i+1)
+                })
 
-            avg_loss += loss.item()
-            data_iter.set_postfix({
-                'avg_loss': avg_loss / (i+1)
-            })
+        y_pred, y_true = [], []
+        with torch.no_grad():
+            model.eval()
+            for X, y in test_loader:
+                X = X.to(device)
 
-    y_pred, y_true = [], []
-    with torch.no_grad():
-        model.eval()
-        for X, y in test_loader:
-            X = X.to(device)
-
-            if model_type == 'regression':
-                preds = model(X).squeeze()
-            else:
                 preds = torch.argmax(model(X), dim=-1)
-            y_pred.extend(preds.cpu().tolist())
-            y_true.extend(y.tolist())
 
-    # TODO: Paper used MSE not accuracy
-    if model_type == 'regression':
-        print(f'Test MSE: {mean_squared_error(y_true, y_pred):.4f}')
-    else:
-        print(y_pred)
-        print(y_true)
+                y_pred.extend(preds.cpu().tolist())
+                y_true.extend(y.tolist())
+
         missed_pred, missed_true = [], []
         for y_p, y_t in zip(y_pred, y_true):
             if y_p != y_t:
                 missed_pred.append(y_p)
                 missed_true.append(y_t)
-        print(f'Test Accuracy: {accuracy_score(y_true, y_pred):.4f}')
-        print(f'Test MSE: {mean_squared_error(y_true, y_pred):.4f}')
-        print(f'Test MAE: {mean_absolute_error(y_true, y_pred):.4f}')
-        print(f'Test Incorrect MAE: {mean_absolute_error(missed_true, missed_pred):.4f}')
 
-    torch.save(model.state_dict(), f'models/{model_name}.pt')
+        acc = accuracy_score(y_true, y_pred)
+        mse = mean_squared_error(y_true, y_pred)
+        mae = mean_absolute_error(y_true, y_pred)
+        mae_missed = mean_absolute_error(missed_true, missed_pred)
 
+        print(f'Test Accuracy: {acc:.4f}')
+        print(f'Test MSE: {mse:.4f}')
+        print(f'Test MAE: {mae:.4f}')
+        print(f'Test Incorrect MAE: {mae_missed:.4f}')
 
-def ordinalize(x, n_classes=5):
-    out = np.zeros(n_classes)
-    for i in range(x):
-        out[i] = 1
-    return out
+        acc_list.append(acc)
+        mse_list.append(mse)
+        mae_list.append(mae)
+        mae_missed_list.append(mae_missed)
+
+        torch.save(model.state_dict(), f'models/{model_name}/{model_name}_{trial}.pt')
+
+    print('-' * 30)
+    print(f'Avg Test Accuracy: {mean(acc_list):.4f} ({stdev(acc_list):.4f})')
+    print(f'Avg Test MSE: {mean(mse_list):.4f} ({stdev(mse_list):.4f})')
+    print(f'Avg Test MAE: {mean(mae_list):.4f} ({stdev(mae_list):.4f})')
+    print(f'Avg Test Incorrect MAE: {mean(mae_missed_list):.4f} ({stdev(mae_missed_list):.4f})')
 
 
 if __name__ == '__main__':
