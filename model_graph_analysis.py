9cc5094d3f46964d7e309c207c6005c858db3122
jmelton22 <jmelton22@gmail.com>
2021-05-03 10:55:33 -0400

    Add normalizing edge weights for graph

--- model_graph_analysis.py
+++ model_graph_analysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import torch
+import torch.nn.functional as F
 import networkx as nx
 
 
@@ -14,8 +15,8 @@ def main():
     hidden_weights = torch.cat([params['hidden_layer.weight'], params['hidden_layer.bias'].unsqueeze(-1)], dim=-1)
     out_weights = torch.cat([params['out_layer.weight'], params['out_layer.bias'].unsqueeze(-1)], dim=-1)
 
-    print(f'hidden layer: {hidden_weights.shape}')
-    print(f'out layer: {out_weights.shape}')
+    # print(f'hidden layer: {hidden_weights.shape}')
+    # print(f'out layer: {out_weights.shape}')
 
     in_dim = hidden_weights.shape[-1]  # Includes bias dimension
     hidden_in_dim = hidden_weights.shape[0]
@@ -30,8 +31,8 @@ def main():
     model_graph.add_nodes_from(hidden_nodes)
     model_graph.add_nodes_from(out_nodes)
 
-    in_hidden_edges = make_weighted_edges(in_nodes, hidden_nodes[:-1], hidden_weights)
-    hidden_out_edges = make_weighted_edges(hidden_nodes, out_nodes, out_weights)
+    in_hidden_edges = make_weighted_edges(in_nodes, hidden_nodes[:-1], hidden_weights, normalize=True)
+    hidden_out_edges = make_weighted_edges(hidden_nodes, out_nodes, out_weights, normalize=True)
 
     model_graph.add_edges_from(in_hidden_edges)
     model_graph.add_edges_from(hidden_out_edges)
@@ -54,11 +55,15 @@ def make_nodes(layer, dim, has_bias=True):
 def make_weighted_edges(in_nodes, out_nodes, weights, normalize=False):
     # TODO: Normalize edge weights (make each neuron sum to 1?)
     weights = weights.T
+    if normalize:
+        weights = F.normalize(weights, dim=-1)
     edges = []
     for i, in_node in enumerate(in_nodes):
         for j, out_node in enumerate(out_nodes):
-            edge_weight = abs(weights[i][j].item())
-            edges.append((in_node, out_node, {'weight': edge_weight, 'distance': 1/edge_weight}))
+            edge_weight = weights[i][j].item()
+            sign = 1 if edge_weight >= 0 else -1
+            edge_weight = abs(edge_weight)
+            edges.append((in_node, out_node, {'weight': edge_weight, 'distance': 1/edge_weight, 'sign': sign}))
 
     return edges
 
